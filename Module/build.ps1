70e363a8139c3877b4a3baeb6e146f2b5ad37514
francisco.nabas <francisconabas@outlook.com>
2022-09-06 18:49:05 -0300

    Fixing PS module build issues

--- Module/build.ps1
+++
@@ -1,8 +0,0 @@
-$module = 'WindowsUtilsModule'
-Push-Location $PSScriptRoot
-
-dotnet build $PSScriptRoot\src -o $PSScriptRoot\output\$module\bin
-Copy-Item "$PSScriptRoot\$module\*" "$PSScriptRoot\output\$module" -Recurse -Force
-
-Import-Module "$PSScriptRoot\Output\$module\$module.psd1"
-Invoke-Pester "$PSScriptRoot\Tests"
