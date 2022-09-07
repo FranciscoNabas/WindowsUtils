1a4f3db33d9929e15ea3d2084a473c99d3f53eab
francisco.nabas <francisconabas@outlook.com>
2022-09-06 21:07:06 -0300

    Fixing workflow and adding output to build

--- Module/src/Build.ps1
+++ Module/src/Build.ps1
@@ -8,11 +8,14 @@ param (
 . dotnet build Module\src\WindowsUtilsModule.csproj --arch x64 --configuration Release --output Module\src\bin\Release
 
 ## Copying license and compressing output
+Write-Output 'Copying license...'
 Copy-Item -Path .\LICENSE -Destination .\Module\src\bin\Release -Force
+Write-Output 'Compressing artifact...'
 Compress-Archive -Path .\Module\src\bin\Release\* -DestinationPath .\WindowsUtilsModule.zip
 
 ## Publishing module
 try {
+    Write-Output 'Trying to publish module...'
     Publish-Module -Path Module\src\bin\Release -NuGetApiKey $PsgSec -Repository PSGallery
 }
-catch { }
+catch { Write-Output "Failed to publish module. $($PSItem.Exception.Message)" }
